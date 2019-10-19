8c18413bc9439da51226b04402b17f247d16c562
Samet <sametciftci@yahoo.com>
2019-10-19 17:43:11 +0300

    Dosyalar eklendi.

---
+++ includes/sidebar.php
@@ -0,0 +1,47 @@
+<div class="app-sidebar colored">
+
+    <div class="sidebar-header">
+        <a class="header-brand" href="index.php">
+            
+            <span class="text">RadioApp</span>
+        </a>
+
+    </div>
+    
+    <div class="sidebar-content">
+        <div class="nav-container">
+            <nav id="main-menu-navigation" class="navigation-main">
+                <div class="nav-item">
+                    <a href="index.php"><i class="ik ik-bar-chart-2"></i><span>Anasayfa</span></a>
+                </div>
+
+                <div class="nav-lavel">Radyolar</div>
+                <div class="nav-item">
+                    <a href="radyolar.php"><i class="ik ik-radio"></i><span>Radyoları Görüntüle</span></a>
+                </div>
+                <div class="nav-item">
+                        <a href="radyo-ekle.php"><i class="ik ik-plus"></i><span>Radyo Ekle</span></a>
+                </div>
+                <div class="nav-lavel">Kategoriler</div>
+                <div class="nav-item">
+                    <a href="kategoriler.php"><i class="ik ik-layers"></i><span>Kategorileri Görüntüle</span></a>
+                </div>
+                <div class="nav-item">
+                        <a href="kategori-ekle.php"><i class="ik ik-plus"></i><span>Kategori Ekle</span></a>
+                </div>
+
+                <div class="nav-lavel">Kullanıcılar</div>
+                <div class="nav-item">
+                    <a href="kullanicilar.php"><i class="ik ik-layers"></i><span>Kullanıcıları Görüntüle</span></a>
+                </div>
+
+                <div class="nav-lavel">Ayarlar</div>
+                <div class="nav-item">
+                    <a href="ayarlar.php"><i class="ik ik-settings"></i><span>Ayarlar</span></a>
+                </div>
+                
+            </nav>
+        </div>
+    </div>
+
+</div>
---
+++ kategoriler.php
@@ -0,0 +1,218 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Data Tables | ThemeKit - Admin Template</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+
+        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
+        
+        <link rel="stylesheet" href="plugins/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+        <link rel="stylesheet" href="plugins/ionicons/dist/css/ionicons.min.css">
+        <link rel="stylesheet" href="plugins/icon-kit/dist/css/iconkit.min.css">
+        <link rel="stylesheet" href="plugins/perfect-scrollbar/css/perfect-scrollbar.css">
+        <link rel="stylesheet" href="plugins/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
+        <link rel="stylesheet" href="dist/css/theme.min.css">
+        <script src="src/js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="wrapper">
+        
+
+            <div class="page-wrap">
+                
+                <?php
+
+                include 'includes/sidebar.php';
+
+                ?>
+
+                <div class="main-content">
+                    <div class="container-fluid">
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="card">
+                                    <div class="card-header"><h3>Kategoriler</h3></div>
+                                    <div class="card-body">
+                                        <table id="data_table" class="table">
+                                            <thead>
+                                                <tr>
+                                                    <th>Id</th>
+                                                    <th class="nosort">Ikon</th>
+                                                    <th>Kategori İsmi</th>
+                                                    <th>Kategori Açıklaması</th>
+                                                    <th class="nosort">&nbsp;</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>001</td>
+                                                    <td><img src="img/users/1.jpg" class="table-user-thumb" alt=""></td>
+                                                    <td>Halk Müziği</td>
+                                                    <td>erich@example.com</td>
+                                                    <td>
+                                                        <div class="table-actions">
+                                                                <a href="kategori-duzenle.php"><i class="ik ik-edit-2"></i></a>
+                                                                <a href="#"><i class="ik ik-trash-2"></i></a>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>002</td>
+                                                    <td><img src="img/users/2.jpg" class="table-user-thumb" alt=""></td>
+                                                    <td>Arabesk</td>
+                                                    <td>jgraham@example.com</td>
+                                                    <td>
+                                                        <div class="table-actions">
+                                                                <a href="kategori-duzenle.php"><i class="ik ik-edit-2"></i></a>
+                                                                <a href="#"><i class="ik ik-trash-2"></i></a>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>003</td>
+                                                    <td><img src="img/users/3.jpg" class="table-user-thumb" alt=""></td>
+                                                    <td>Rock</td>
+                                                    <td>grant.simonis@example.com</td>
+                                                    <td>
+                                                        <div class="table-actions">
+                                                                <a href="kategori-duzenle.php"><i class="ik ik-edit-2"></i></a>
+                                                                <a href="#"><i class="ik ik-trash-2"></i></a>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+     
+                    </div>
+                </div>
+ 
+      
+                <footer class="footer">
+                    <div class="w-100 clearfix">
+                        <span class="text-center text-sm-left d-md-inline-block">Copyright © 2018 ThemeKit v2.0. All Rights Reserved.</span>
+                        <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Crafted with <i class="fa fa-heart text-danger"></i> by <a href="http://lavalite.org/" class="text-dark" target="_blank">Lavalite</a></span>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        
+        
+        
+
+        <div class="modal fade apps-modal" id="appsModal" tabindex="-1" role="dialog" aria-labelledby="appsModalLabel" aria-hidden="true" data-backdrop="false">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><i class="ik ik-x-circle"></i></button>
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="quick-search">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-4 ml-auto mr-auto">
+                                    <div class="input-wrap">
+                                        <input type="text" id="quick-search" class="form-control" placeholder="Search..." />
+                                        <i class="ik ik-search"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-body d-flex align-items-center">
+                        <div class="container">
+                            <div class="apps-wrap">
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-bar-chart-2"></i><span>Dashboard</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-mail"></i><span>Message</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-users"></i><span>Accounts</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-shopping-cart"></i><span>Sales</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-briefcase"></i><span>Purchase</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-server"></i><span>Menus</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-clipboard"></i><span>Pages</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-message-square"></i><span>Chats</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-map-pin"></i><span>Contacts</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-box"></i><span>Blocks</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-calendar"></i><span>Events</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-bell"></i><span>Notifications</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-pie-chart"></i><span>Reports</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-layers"></i><span>Tasks</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-edit"></i><span>Blogs</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-settings"></i><span>Settings</span></a>
+                                </div>
+                                <div class="app-item">
+                                    <a href="#"><i class="ik ik-more-horizontal"></i><span>More</span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script>window.jQuery || document.write('<script src="src/js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+        <script src="plugins/popper.js/dist/umd/popper.min.js"></script>
+        <script src="plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="plugins/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
+        <script src="plugins/screenfull/dist/screenfull.js"></script>
+        <script src="plugins/datatables.net/js/jquery.dataTables.min.js"></script>
+        <script src="plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+        <script src="dist/js/theme.min.js"></script>
+        <script src="js/datatables.js"></script>
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
